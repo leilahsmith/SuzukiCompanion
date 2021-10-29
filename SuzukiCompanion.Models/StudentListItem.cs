2d16d5e6e96f01f8e5fc41e77d3b897a816242c8
leilahsmith <leilah.sdev@gmail.com>
2021-10-29 14:04:59 -0300

    Added StudentListItem and StudentController

---
+++ SuzukiCompanion.Models/StudentListItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuzukiCompanion.Models
+{
+    public class StudentListItem
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstName; }
+        }
+        public DateTimeOffset BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Location { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
---
+++ SuzukiCompanion.WebMVC/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using SuzukiCompanion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuzukiCompanion.WebMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        [Authorize]
+        public ActionResult Index()
+        {
+            var model = new StudentListItem[0];
+            return View(model);
+        }
+    }
+}
